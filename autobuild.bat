eb8dbde06dbed37d81550a6a8acceaac4ed0d69d
fishjam <fish.jam@navercorp.com>
2019-07-07 15:24:47 +0800

    commit for windows build successful

--- autobuild.bat
+++ autobuild.bat
@@ -1,12 +1,26 @@
-@echo on
+@echo off
 setlocal 
 
 set ROOT_DIR=%~dp0
+set ACTION=
+
+set CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
 set BUILD_LEPTONICA=1
 set LEPTONICA_BRANCH=branch_1.74.4
-
 set BUILD_TESSERACT=1
 set TESSERACT_BRANCH=3.05
+set BUILD_OPENCV=1
+OPENCV_BRANCH=branch_3.4.2_fixbug
+
+if not "%1" == "" set ACTION=%1
+if "%ACTION%" == "clean" (
+  cd %ROOT_DIR%FFmpeg && rm -rf build
+  cd %ROOT_DIR%opencv && rm -rf build
+  cd %ROOT_DIR%leptonica && rm -rf build
+  cd %ROOT_DIR%tesseract && rm -rf build
+  goto CLEAN
+)
+
 
 if "%BUILD_LEPTONICA%" == "1" (
     rem now will build for leptonica
@@ -19,12 +33,13 @@ if "%BUILD_LEPTONICA%" == "1" (
     if not "%ERRORLEVEL%" == "0" goto ERROR
 
     cmake ^
-        -G "Visual Studio 14 2015 Win64" ^
+        -G %CMAKE_GENERATOR% ^
         -DCMAKE_INSTALL_PREFIX=%ROOT_DIR%result\win ^
         ..
         
     if not "%ERRORLEVEL%" == "0" goto ERROR
 
+    rem "%VS140COMNTOOLS%/../IDE/devenv.com" videoAnalyzer.sln /build "%BUILD_TYPE%|x64"
     cmake --build . --config Release --target INSTALL
     if not "%ERRORLEVEL%" == "0" goto ERROR
     
@@ -39,7 +54,7 @@ if "%BUILD_TESSERACT%" == "1" (
     if not "%ERRORLEVEL%" == "0" goto ERROR
 
     cmake ^
-        -G "Visual Studio 14 2015 Win64" ^
+        -G %CMAKE_GENERATOR% ^
         -DBUILD_TRAINING_TOOLS=OFF ^
         -DLeptonica_Dir=%ROOT_DIR%result\win\cmake ^
         -DCMAKE_INSTALL_PREFIX=%ROOT_DIR%result\win ^
@@ -53,7 +68,34 @@ if "%BUILD_TESSERACT%" == "1" (
     cd %ROOT_DIR%
 )
 
-goto Done
+if "%BUILD_OPENCV%" == "1" (
+    rem now will build for opencv
+    cd opencv
+    git checkout %OPENCV_BRANCH%
+    if not "%ERRORLEVEL%" == "0" goto ERROR
+    
+    mkdir build
+    cd build
+    if not "%ERRORLEVEL%" == "0" goto ERROR
+
+    cmake ^
+        -G %CMAKE_GENERATOR% ^
+        -DCMAKE_INSTALL_PREFIX=%ROOT_DIR%result\win ^
+        -DBUILD_SHARED_LIBS=ON ^
+        -DBUILD_opencv_world=ON ^
+        -DBUILD_TESTS=OFF ^
+        -DBUILD_EXAMPLES=OFF ^
+        ..
+        
+    if not "%ERRORLEVEL%" == "0" goto ERROR
+
+    cmake --build . --config Release --target INSTALL
+    if not "%ERRORLEVEL%" == "0" goto ERROR
+    
+    cd %ROOT_DIR%
+)
+
+goto DONE
 
 :ERROR
 cd %ROOT_DIR%
@@ -63,9 +105,18 @@ endlocal
 pause
 @exit /b 1
 
-:Done
+:CLEAN
+cd %ROOT_DIR%
+endlocal
+@echo ***************************************************
+@echo Clean Process Done...
+@echo ***************************************************
+@exit /b 0
+
+:DONE
 endlocal
 @echo ***************************************************
 @echo Build Process Done...
 @echo ***************************************************
 pause
+@exit /b 0
